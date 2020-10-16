b8a2700d9a8dad2270044b7986abe2de1fdb3b09
Frytz <dacook1983@gmail.com>
2020-10-15 23:39:51 -0400

    remade app foro deploy testing

---
+++ build/precache-manifest.f25f2f8df45730aec02f95fcf218c88a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbc08d07cfb6a74e817c0296a84beadb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4620d1e7aafd02aec044",
+    "url": "/static/css/main.5a7c2f91.chunk.css"
+  },
+  {
+    "revision": "a683979d428e851f229c",
+    "url": "/static/js/2.c126451c.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.c126451c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4620d1e7aafd02aec044",
+    "url": "/static/js/main.c8fa0ce5.chunk.js"
+  },
+  {
+    "revision": "b3bea7fa12fe64b6bf1e",
+    "url": "/static/js/runtime-main.16caac28.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
